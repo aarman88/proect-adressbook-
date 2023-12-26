75d1eab84688fffdd757f3d895e2fe8968b80c12
aarman88 <147742499+aarman88@users.noreply.github.com>
2023-12-26 11:54:13 +0600

    Update Program.cs

--- proect(adressbook)/Program.cs
+++ proect(adressbook)/Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace proect_adressbook_
 {
+    
+    // Класс Program содержит точку входа в приложение.
     class Program
     {
+        // Метод Main запускает менеджер адресной книги для взаимодействия с пользователем.
         static void Main()
         {
+            // Создание и запуск менеджера адресной книги.
             AddressBookManager addressBookManager = new AddressBookManager();
             addressBookManager.Run();
         }
     }
+
 
 }
