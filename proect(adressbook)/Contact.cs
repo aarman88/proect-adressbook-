bd81a6c93229a9232dbbd63c85a6ac783d741cb9
aarman88 <147742499+aarman88@users.noreply.github.com>
2023-12-26 11:53:55 +0600

    Contact.cs

--- proect(adressbook)/Contact.cs
+++ proect(adressbook)/Contact.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,20 @@ namespace proect_adressbook_
 {
     public class Contact
     {
+        // Свойство для хранения 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
 
+
+        // Переопределение метода ToString для удобного форматированного вывода контакта.
         public override string ToString()
         {
             return $"{FirstName} {LastName} - {PhoneNumber} - {Email} - {Address}";
         }
     }
+
 
 }
